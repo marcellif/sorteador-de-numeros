f7f92f453bf8ccd0854280ea763aa0b0a4b7f367
marcelli <marcelli.faria@gmai.com>
2024-06-18 20:35:31 -0300

    implementando reiniciação de jogo

--- app.js
+++ app.js
@@ -13,11 +13,36 @@ function sorteia(){
             }
             listaSorteados.push(numero);
         }
-        
+
     let mensagem = document.getElementById('resultado');
     mensagem.innerHTML = `<label class="texto__paragrafo"> Numeros sorteados: ${listaSorteados} </label>`
+
+    alterarStatusBotao ();
 }
 
 function numerosAleatoriosEntreLimites (min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao(){
+    let botao = document.getElementById('btn-reiniciar');
+    
+    if (botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    }else{
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+
+}
+
+function reinicia (){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value ='';
+    document.getElementById('ate').value = '';
+
+    document.getElementById('resultado').innerHTML = `<label class="texto__paragrafo"> Numeros sorteados: nenhum até agora </label>`
+
+    alterarStatusBotao();
 }
