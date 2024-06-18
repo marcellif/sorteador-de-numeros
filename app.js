e426fba8b8392cba1cd9ef9d6b48b4d383e23df0
marcelli <marcelli.faria@gmai.com>
2024-06-17 21:38:06 -0300

    iniciando o projeto

---
+++ app.js
@@ -0,0 +1,7 @@
+function sorteia(){
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value
+
+    alert(`quantidade de números:${quantidade}, do número ${de} até o número ${ate}`);
+}
